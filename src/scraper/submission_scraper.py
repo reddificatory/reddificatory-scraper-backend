a694e059b7f60dd11236ebba45bb3cfc8847b8b5
rokuwu <rookuczi@gmail.com>
2023-02-24 09:54:54 +0100

    refactored code

--- src/scraper/submission_scraper.py
+++ src/scraper/submission_scraper.py
@@ -3,62 +3,33 @@ sys.path.insert(0, 'D:\coding\python-reddit-tts\src\db')
 sys.path.insert(0, 'D:\coding\python-reddit-tts\src')
 
 import config
-from submissions import store_submissions
+from submissions import store_submission
 import praw
-import pandas as pd
-import nltk
 from nltk.sentiment.vader import SentimentIntensityAnalyzer as SIA
 
 REDDIT = praw.Reddit(client_id = config.CLIENT_ID, client_secret = config.CLIENT_SECRET, user_agent = config.CLIENT_AGENT)
 
-def collect_submissions(subreddit):
+def decide_if_strong(polarity_score):
+    if polarity_score['compound'] > 0.5 or polarity_score['compound'] < -0.5:
+        return True
+    
+    return False
+
+def analyze_submission(submission):
+    sentiment_instensity_analyzer = SIA()
+
+    polarity_score = sentiment_instensity_analyzer.polarity_scores(submission.title)
+    return decide_if_strong(polarity_score)
+
+def scrape_subreddit(client, subreddit):
+    subreddit = client.subreddit(subreddit)
     submissions = set()
+    
     for submission in subreddit.hot(limit = 50):
-        submissions.add(submission)
+        if analyze_submission(submission):
+            submissions.add(submission)
+            store_submission(submission)
 
     return submissions
 
-def decide_strong_submissions(scores):
-    df = pd.DataFrame(scores)
-    
-    for score in scores:
-        df['strong'] = False
-        df.loc[df['compound'] > 0.5, 'strong'] = True 
-        df.loc[df['compound'] < -0.5, 'strong'] = True
-
-    return df    
-
-def get_strong_submissions(df):
-    df_dict = df.to_dict()
-    strongs = []
-    i = 0
-
-    for x in df_dict['strong']:
-        if df_dict['strong'][i]:
-            strongs.append(df_dict['submission'][i])
-
-        i = i + 1
-
-    return strongs
-
-def analize_submissions(submissions):
-    sia = SIA()
-    scores = []
-
-    for submission in submissions:
-        pol_score = sia.polarity_scores(submission.title)
-        pol_score['submission'] = submission
-        scores.append(pol_score)
-
-    decided = decide_strong_submissions(scores)
-    strongs = get_strong_submissions(decided)
-
-    return strongs
-
-def scrape_subreddit(subreddit):
-    subreddit = REDDIT.subreddit(subreddit)
-    submissions = collect_submissions(subreddit)
-
-    return analize_submissions(submissions)
-
-store_submissions(scrape_subreddit('askreddit'))
+print(scrape_subreddit(REDDIT, 'askreddit'))
